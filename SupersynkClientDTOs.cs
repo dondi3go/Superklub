daff89d98af93fd345acd8b862916331ed048b50
eric <fake@fake.com>
2023-07-31 16:14:28 +0200

    Fix preprocessing for Unity Build

--- SupersynkClientDTOs.cs
+++ SupersynkClientDTOs.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 #if UNITY_STANDALONE
